25df9d0a133ace8178b13f225d60f6438b486625
cody-proo <cody18v@gmail.com>
2022-09-18 15:11:13 +0430

    rate limiter config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import { config } from "./config";
+import rateLimiter from "express-rate-limit";
 import { AppRoutes } from "./config/routes.config";
 
 const bootstrap = () => {
   const app = express();
   app.use(express.json());
+  app.use(
+    rateLimiter({
+      // waiting time after get error
+      windowMs: 60 * 1000 * 1,
+      // maximum number of request that receive from ip
+      max: 10,
+      legacyHeaders: false,
+    })
+  );
 
   // Config All Routes In App
   new AppRoutes(app).config();
