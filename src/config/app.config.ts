1634f56bb70ac335fc63198c5ab5e59d4d517dad
cody-proo <cody18v@gmail.com>
2022-09-20 00:04:15 +0430

    fix all bugs

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import rateLimiter from "express-rate-limit";
+import compression from "compression";
+import cors from "cors";
 import { AppRoutes } from "./routes.config";
 
 const configApp = () => {
   const app = express();
+  app.use(cors());
   app.use(express.json());
+  app.use(compression());
   app.use(
     rateLimiter({
       // waiting time after get error
